b0a84c5b70fba335353c7dd7fe54baef79e0e8bc
cafewang <434637878@qq.com>
2025-10-23 00:17:09 +0800

    complete interrupt thread

--- platform/linux/intr.c
+++ platform/linux/intr.c
@@ -65,24 +65,81 @@ inline int init_irq_entry(struct irq_entry *entry, unsigned int irq, intr_handle
 int
 intr_raise_irq(unsigned int irq)
 {
+    return pthread_kill(tid, irq);
 }
 
 static void *
 intr_thread(void *arg)
 {
+    int terminate = 0, sig, err;
+    struct irq_entry *entry;
+
+    debugf("interrupt thread started, tid=%lu", pthread_self());
+    pthread_barrier_wait(&barrier);
+
+    while (!terminate) {
+        err = sigwait(&sigmask, &sig);
+        if (err) {
+            errorf("sigwait() failure, err=%s", strerror(err));
+            break;
+        }
+        debugf("signal received, sig=%d", sig);
+        if (sig == SIGHUP) {
+            debugf("interrupt thread terminating");
+            terminate = 1;
+            continue;
+        }
+        for (entry = irqs; entry; entry = entry->next) {
+            if (entry->irq == (unsigned int)sig) {
+                debugf("invoking handler for irq=%u, name=%s", entry->irq, entry->name);
+                entry->handler(entry->irq, entry->dev);
+            }
+        }
+    }
+    debugf("interrupt thread exited");
+    return NULL;
 }
 
 int
 intr_run(void)
 {
+    // block signals in the main thread
+    int err = pthread_sigmask(SIG_BLOCK, &sigmask, NULL);
+    if (err) {
+        // strerror illustrates the error code
+        errorf("pthread_sigmask() failure, err=%s", strerror(err));
+        return -1;
+    }
+    // create interrupt handling thread
+    err = pthread_create(&tid, NULL, intr_thread, NULL);
+    if (err) {
+        errorf("pthread_create() failure, err=%s", strerror(err));
+        return -1;
+    }
+    // wait for the thread to be ready
+    pthread_barrier_wait(&barrier);
+    return 0;
 }
 
 void
 intr_shutdown(void)
 {
+    if (pthread_equal(tid, pthread_self())) {
+        // intr_thread not created
+        return;
+    }   
+    // send a sighup signal to interrupt thread
+    pthread_kill(tid, SIGHUP);
+    pthread_join(tid, NULL);
 }
 
 int
 intr_init(void)
 {
+    tid = pthread_self();
+    // set countdown to 2
+    pthread_barrier_init(&barrier, NULL, 2);
+    sigemptyset(&sigmask);
+    sigaddset(&sigmask, SIGHUP);
+    return 0;
 }
